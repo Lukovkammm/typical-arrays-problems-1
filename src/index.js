9414ef9c30541d2bda4df4f3d88b100c7a452ee0
Marina <artyusmarina@yandex.ru>
2021-03-06 11:03:58 +0300

    feat: add code for task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array === undefined || array == 0 || array == Infinity) return 0;
+    return  (Math.min(...array))
+ 
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array === undefined || array == 0 || array == Infinity) return 0;
+  let num = 0;
+  array.forEach(item => {
+      if (item > num) num = item;
+  })
+  return num;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined || array == 0 || array == Infinity) return 0;
+    return (array.reduce((acc, value) => acc+value)) / array.length ;
 }
